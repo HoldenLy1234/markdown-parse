ea4685f921a21c29dde05060cc005f5aadf3d0b6
HoldenLy1234 <98137229+HoldenLy1234@users.noreply.github.com>
2022-02-03 10:32:36 -0800

    junit

---
+++ MarkdownParseTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Test;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(99999999, 1 + 1);
+    }
+    @Test
+   public void checkContent() {
+    
+       try {
+           Path filePath = Path.of("test-file3.md");
+           String contents = Files.readString(filePath);
+           assertEquals("", contents);
+        
+       } catch(Exception e) {
+            System.out.println(e.toString());
+       }
+   }
+   @Test
+   public void jTestPlat() {
+    
+       try {
+           Path filePath = Path.of("test-file3.md");
+           String contents = Files.readString(filePath);
+           assertEquals("hi", contents);
+        
+       } catch(Exception e) {
+            System.out.println(e.toString());
+       }
+   }
+}
